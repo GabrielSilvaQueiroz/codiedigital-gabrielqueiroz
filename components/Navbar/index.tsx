d4da2e6ab447a5414bf7e3a0a78193270882b8a6
Gabriel Queiroz <gabrielsq@yahoo.com.br>
2024-02-01 16:08:12 -0300

    ajustando css finais

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
 import Link from 'next/link'
 import Image from 'next/image'
 import WhitePokeboll from '../../public/images/white-pokeball.svg'
 import { IconsNavbar, ButtonImage, DivNavbar, ListaItems, Items, ButtonAgendarConsultar, ButtonQuemSomos } from './style'
+import React, { useState, useEffect } from 'react';
 
 export default function Navbar() {
+
+    // Cria um estado para gerenciar se o texto do botão deve ser ocultado ou exibido.
+    const [ocultarTexto, setOcultarTexto] = useState(false);
+
+    // useEffect para ocultar automaticamente o texto após 5 segundos.
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setOcultarTexto(true);
+        }, 5000);
+
+        // Função de limpeza para cancelar o timeout quando o componente é desmontado
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+
+    // Para mostrar o texto no botão.
+    const handleMouseEnter = () => {
+        setOcultarTexto(false);
+    };
+
+    // Para ocultar o texto no botão.
+    const handleMouseLeave = () => {
+        if (!ocultarTexto) {
+            setOcultarTexto(true);
+        }
+    };
+
     return (
         <>
             <IconsNavbar>
                 <DivNavbar>
-                    <Link href='/'>  <ButtonImage> <Image src={WhitePokeboll} alt='White-Pokeboll' width="27" height="23" /> Centro Pokémon </ButtonImage> </Link>
+                    <Link href='/'>
+                        <ButtonImage ocultarTexto={ocultarTexto} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> {/* Adicionar onMouseEnter */}
+                            <Image src={WhitePokeboll} alt='White-Pokeboll' width="27" height="23" />
+                            {!ocultarTexto && <span>Centro Pokémon</span>}
+                        </ButtonImage>
+                    </Link>
 
                     <ListaItems>
                         <Items>
--- components/Navbar/style.ts
+++ components/Navbar/style.ts
@@ -1,4 +1,10 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+
+interface ButtonImageProps {
+    ocultarTexto: boolean;
+}
+
 
 export const IconsNavbar = styled.nav`
 display: flex;
@@ -10,27 +16,42 @@ background-color: #FFFFFF;
 color: black;
 `
 
-export const ButtonImage = styled.button`
-background-color: #E40F10;
-color: white;
-display: flex;
-align-items: center;
-border: none;
-border-radius: 30px;
-width: 155px;
-height: 39px;
-gap: 7px;
-cursor: pointer;
+export const ButtonImage = styled.button<ButtonImageProps>`
+    background-color: #E40F10;
+    color: white;
+    display: flex;
+    align-items: center;
+    border: none;
+    border-radius: 30px;
+    width: ${(props) => (props.ocultarTexto ? '38px' : '155px')};
+    height: ${(props) => (props.ocultarTexto ? '39px' : '39px')};
+    gap: 7px;
+    cursor: pointer;
 
-img {
-    margin-left: 5px ;
+    img {
+        margin-left: 5px;
+    }
+
+    span {
+    display: block;
+    color: white;
+    opacity: ${(props) => (props.ocultarTexto ? '0' : '1')};
+    transition: opacity 0.3s ease;  // Adicione esta linha para uma transição suave
 }
 
-&:active {
-    opacity: 0.4;
+&:hover {
+    width: 155px;
+    height: 39px;
+
+    span {
+        opacity: 1;
+    }
 }
 
-`
+    &:active {
+        opacity: 0.4;
+    }
+`;
 
 export const DivNavbar = styled.div`
 width: 100%;
